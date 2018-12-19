80ba8df541d76e81d5cad67e293d24cfcf198e47
Lucas Calmon <lucas.calmon11@gmail.com>
2018-12-19 03:53:10 +0000

    Second try boy

---
+++ Source/FinalProj/Public/GameModeMultiplayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GameModeMultiplayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALPROJ_API AGameModeMultiplayer : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
